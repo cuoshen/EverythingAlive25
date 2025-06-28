d69e3e629fc9f9d605efe6058112a841f0185c5c
Mark_Z <yz4602@tc.columbia.edu>
2025-06-28 18:03:59 +0800

    Update GameState

--- Alive25/Assets/MarkDev/GameState.cs
+++ Alive25/Assets/MarkDev/GameState.cs
@@ -6,6 +6,14 @@ using UnityEngine;
 public class GameState : SingletonMono<GameState>
 {
     public E_GameStateType currentGameType;
+    public float Player1HP = 100;
+    public float Player2HP = 100;
+    public float Player1Energy = 10;
+    public float Player2Energy = 10;
+    
+    private float PlayerMaxHP = 100;
+    private float PlayerMaxEnergy = 10;
+    
     
     // Start is called before the first frame update
     // void Start()
